c1540c2e0c5148a8adfa16f19cdde1deec55119a
Alex Dworjan <adworjan@redhat.com>
2023-12-20 10:12:52 -0500

    add powershell script

---
+++ alert.ps1
@@ -0,0 +1,7 @@
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Content-Type", "application/json")
+
+$body = "{`"vmware_alert`": {`n `"title`": `"vCenter Alarm`",`n `"target`": `"$env:VMWARE_ALARM_TARGET_NAME`",`n `"value`": `"$env:VMWARE_ALARM_ALARMVALUE`",`n `"status`": `"$env:VMWARE_ALARM_NEWSTATUS`",`n `"alarm`": `"$env:VMWARE_ALARM_NAME`",`n `"description`": `"$env:VMWARE_ALARM_EVENTDESCRIPTION`"`n }`n}"
+
+$response = Invoke-RestMethod 'http://eda.shadowman.dev:5000/endpoint' -Method 'POST' -Headers $headers -Body $body
+$response | ConvertTo-Json

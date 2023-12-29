70086b9342e16ff620f70c16e6b757799698e6f3
Alex Dworjan <adworjan@redhat.com>
2023-12-29 11:10:49 -0500

    improved readability

--- alert.ps1
+++ alert.ps1
@@ -4,7 +4,8 @@ $headers.Add("Content-Type", "application/json")
 $body = "{`"vmware_alert`": {`n `"title`": `"vCenter Alarm`",
 `n `"target`": `"$env:VMWARE_ALARM_TARGET_NAME`",
 `n `"value`": `"$env:VMWARE_ALARM_ALARMVALUE`",
-`n `"status`": `"$env:VMWARE_ALARM_NEWSTATUS`",`n `"alarm`": `"$env:VMWARE_ALARM_NAME`",
+`n `"status`": `"$env:VMWARE_ALARM_NEWSTATUS`",
+`n `"alarm`": `"$env:VMWARE_ALARM_NAME`",
 `n `"description`": `"$env:VMWARE_ALARM_EVENTDESCRIPTION`"`n }`n}"
 
 $response = Invoke-RestMethod 'http://eda.shadowman.dev:5000/endpoint' -Method 'POST' -Headers $headers -Body $body
